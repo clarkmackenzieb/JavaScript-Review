de8730f47e883335c8424bbf3e7500f3a535804d
Mackenzie <clarkmackenzieb@gmail.com>
2017-08-29 14:59:32 -0500

    updated work on practice

--- arrayReview.js
+++ arrayReview.js
@@ -4,7 +4,9 @@ var threeItems = [1,2,3];
 
   //code here
 
-
+function last(arr){
+  return arr[arr.length-1];
+}
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
@@ -15,6 +17,15 @@ var evenArray = [1,2,3,6,22,98,45,23,22,12];
 
   //code here
 
+  function oddRemove(arr){
+    for(var i = arr.length+1; i >= 0; i--){
+      if(arr[i]%2 !== 0){
+        arr.splice(i, 1);
+      }
+    }
+    return arr;
+  }
+  
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
@@ -28,6 +39,16 @@ var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 
   //code here
 
+function checkMeOut(arr){
+  var ranNum = getRandomArbitrary();
+  var checky = false;
+  for(var i = 0; i < arr.length; i++){
+    if(arr[i] === ranNum){
+      checky = true;
+    }
+  }
+  return checky;
+}
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
@@ -38,6 +59,11 @@ var first = [1,2,3,4,5];
 var second;
 
   //code here
+
+  second = first.slice(0);
+
+  second.push(6);
+  second.push(7);
 
 alert(first) //[1,2,3,4,5];
 alert(second) //[1,2,3,4,5,6,7];
@@ -52,7 +78,17 @@ alert(second) //[1,2,3,4,5,6,7];
 var sentence = "Dev Mountain is the best"
 
   //code here
-
+  function longest(str){
+    var newArr = str.split(" ");
+    var county = "";
+    for(var i = 0; i < newArr.length; i++){
+      if(newArr[i].length > county.length){
+        county = newArr[i];
+      }
+    }
+    return county;
+  }
+  
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
@@ -64,6 +100,17 @@ var myPoem = 'What is a jQuery but a misunderstood object?'
 
   //code here
 
+  function capitalize(str){
+    var newArr = str.split(" ");
+    var newVar = "";
+    newArr.map(function(word){
+      newVar += word.charAt(0).toUpperCase() + word.substring(1) + " ";
+      // console.log(newVar);
+    });
+    return newVar;
+
+}
+
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
@@ -72,3 +119,8 @@ var myPoem = 'What is a jQuery but a misunderstood object?'
 
 var theOdyssey = "function expression or function declaration? Tis an obvious choice";
 //Write a function called vowelCounter that takes in a string (theOdyssey) and returns how many vowels are in that string.
+
+function vowelCounter(str){
+  var county = str.replace(/[^aeiou]/gi, "") //THANKS JAMES
+  return county.length;
+}
--- constructorReview.js
+++ constructorReview.js
@@ -1,42 +1,77 @@
 //Create a Animal Constructor that has the following parameters. species, name, legs, color, food (which is an array of foods they can eat).
+function Animal(species,name,legs,color,food){
 
   //code here
 
-
+  this.species = species;
+  this.name = name;
+  this.legs = legs;
+  this.color = color;
+  this.food = food;
+}
 //Now create a person function that creates an object and returns it (not in constructor form) that has the following parameters. name, age, height, gender
 
   //code here
+function person(name, age, height, gender){
+  return {
+    name: name,
+    age: age,
+    height: height,
+    gender: gender,
 
+  }
 
-//Create a animal array and a person array.
+}
+
+//Create an animal array and a person array.
 
   //code here
-
+var animals = [];
+var person = [];
 
 //Create two instances of Animal and push those into your animal array
 
   //code here
+var cat = new Animal("cat", "delilah", 4, "calico", ["fish", "birds", "moles"]);
 
+var dog = new Animal("dog", "dudley", 4, "golden", ["dog food", "trash", "cheeseburgers"])
+
+animals.push(dog);
+animals.push(cat);
 
 //Create two instances of person and push those into your person array.
 
   //code here
 
+  var Mackenzie = person("Mackenzie", 22, "SUPER SHORT", "Lady");
+  var Paul = person("Paul", 25, "a lil short", "Dude");
+
+  people.push(Mackenzie);
+  people.push(Paul);
 
 //Now we want every instance of Animal to have a eat method. This method will choose a random item in that instances food array, then alert "(name) ' ate ' (whichever food was chosen)".
 
   //code here
 
+  Animal.prototype.eat = function(){
+    var num = (Math.random() * (this.food.length - 0 + 1)) + 0;
+    alert(Animal.name + "ate" + Animal.food[num]);
+  }
 
 //At this point, if we wanted to add something to every istance of person could we? 
 
   //Yes or no? and why or why not?
 
+  //no, because it has no contextual bindings 
 
 
 /*
   1) What happens when you use the 'new' keyword to call a Constructor function?
+  // it creates a new instance/object with the passed in arguments
   2) What's a good way to describe the keyword 'this'
+  // a pointer to objects. defines context.
   3) Can a normal function which creates an object and then returns that object use the prototype?
+  // yes, but... it's weird. so don't do it. 
   4) What happens if you forget to use 'new' when calling a constructor?
+  // :O binds to window. bad! don't do it!
 */
